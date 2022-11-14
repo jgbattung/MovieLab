08672d3182ee809c51b27e9fc6a28c96cf21e3e4
Jireh Battung <jirehgbattung@gmail.com>
2022-11-14 12:22:35 +0800

    added title component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { useEffect  } from "react";
 import Navbar from "./components/Navbar";
 import SearchMovies from "./components/SearchMovies";
+import Title from "./components/Title";
 
 function App() {
   return (
     <div className="container">
       <Navbar />
+      <Title />
       <SearchMovies/>
     </div>
   );
---
+++ src/components/Title.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Title = () => {
+  return (
+    <div>MovieLab</div>
+  )
+}
+
+export default Title
